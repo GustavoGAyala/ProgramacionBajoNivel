f397322cc36fad351f15f47eadffee2c6e111b3b
Gustavo Ayala <gustavoayala25@gmail.com>
2023-05-04 19:05:32 -0300

    funcion para inicializar la lista en NULL

--- listaEnlazada.c
+++ listaEnlazada.c
@@ -7,12 +7,48 @@
 
 #include <stadio.h>
 #include <stdlib.h>
-
+/*IMPLEMENTACION DE UN NODO Y SU SIGUIENTE*/
 typedef struct Nodo{
     int valor;
     struct Nodo *sig;
 
 }Nodo;
+
+/*El primer elemento, el último elemento, el número de elementos.
+ * Para ello será utilizado otra estructura "Lista"
+*/
+        typedef struct ListaIdentificar {
+
+            Nodo *inicio;
+
+            Nodo *fin;
+
+            int tamaño;
+
+        }Lista;
+
+  /*El puntero inicio contendrá la dirección del primer elemento de la lista.
+   * El puntero fin contendrá la dirección del último elemento de la lista.
+   * La variable tamaño contiene el número de elementos. Cualquiera que sea la posición en la lista,
+   * los punteros inicio y fin apuntan siempre al primer y último elemento.
+   * El campo tamaño contendrá el numero de elementos de la lista cualquiera
+   * que sea la operación efectuada sobre la lista.
+   */
+/*
+ * Esta operación debe ser hecha antes de cualquier otra operación sobre la lista.
+ * Esta inicializa el puntero inicio y el puntero fin con el puntero NULL, y el tamaño con el valor 0.
+*/
+void inicializacion (Lista *lista){
+
+    lista->inicio = NULL;
+
+    lista->fin = NULL;
+
+    tamaño = 0;
+
+}
+
+
 
 int main(){
     //reservar espacio para los nodos
