1130d7e1dec63ce2780d1516137fa211ad0fd65d
Gustavo Ayala <gustavoayala25@gmail.com>
2023-05-14 14:18:41 -0300

    crearPersona con fallas, comienzo funcion eliminarPersona

--- listaEnlazada.c
+++ listaEnlazada.c
@@ -9,42 +9,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-
-/*El primer elemento, el último elemento, el número de elementos.
- * Para ello será utilizado otra estructura "Lista"
-*//*
-typedef struct ListaIdentificar {
-
-    Nodo *inicio;
-
-    Nodo *fin;
-
-    int tamaño;
-
-}Lista;*/
-
-  /*El puntero inicio contendrá la dirección del primer elemento de la lista.
-   * El puntero fin contendrá la dirección del último elemento de la lista.
-   * La variable tamaño contiene el número de elementos. Cualquiera que sea la posición en la lista,
-   * los punteros inicio y fin apuntan siempre al primer y último elemento.
-   * El campo tamaño contendrá el numero de elementos de la lista cualquiera
-   * que sea la operación efectuada sobre la lista.
-   */
-/*
- * Esta operación debe ser hecha antes de cualquier otra operación sobre la lista.
- * Esta inicializa el puntero inicio y el puntero fin con el puntero NULL, y el tamaño con el valor 0.
-*//*
-void inicializacion (Lista *lista){
-
-    lista->inicio = NULL;
-
-    lista->fin = NULL;
-
-    tamaño = 0;
-
-}*/
-
 /*
 Parte 1:
 Hacer la implementación de una lista enlazada.
@@ -56,83 +20,52 @@ Debe haber funciones para:
 -Eliminar un elemento N de la lista
 -Imprimir la lista
 */
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wpointer-to-int-cast"
-/*IMPLEMENTACION DE UN NODO Y SU SIGUIENTE*/
-typedef struct{
+
+/*IMPLEMENTACION DE UN NODO (Persona/Cliente) Y SU SIGUIENTE*/
+typedef struct Persona{
     char name;
     int edad;
-
-
+    struct Persona *sig;
 }Persona;
-typedef struct{
-    Persona p;
-    struct NodoPersona *sig;
-}NodoPersona;
-
 /*Creacion  de un Nodo (persona/cliente)*/
-NodoPersona *crearNodo(char nombre,int edad){
-    NodoPersona *aux = (NodoPersona*) malloc(sizeof(NodoPersona));
-    aux->p.edad = edad;
-    aux->p.name = scanf("%s",nombre);
-    /*aux->p.name = *gets(nombre);*/
+Persona *crearPersona(char nombre, int edad){
+    Persona *aux;
+    aux = malloc(sizeof(Persona));
+    aux->edad = edad;
+    aux->name = scanf((const char *) nombre);
+    /*aux->name = *gets(nombre);*/
     aux->sig = NULL;
     return aux;
-
 }
-
-/*Inicia con un Puntero de tipo Nodo que apunta  NULL, el inicio de la lista queda creada y vacia*/
-NodoPersona inicializarLista() {
-    return NULL;
-}
-
-void agregarNodoALista (NodoPersona **lista, NodoPersona *nuevo) {
-    if(nuevo == NULL) {
-        *lista = nuevo;
+void agregarNodoALista (Persona **lista, Persona *nuevaPersona) {
+    if(nuevaPersona == NULL) {
+        *lista = nuevaPersona;
     }
     else {
-        nuevo->sig = *lista;
-        *lista = nuevo;
+        nuevaPersona->sig = *lista;
+        *lista = nuevaPersona;
     }
-
 }
-void imprimirLista (NodoPersona **lista) {
+void eliminarElemntoDeLaLista(Persona **lista, Persona *quienElimina){
+    if(*lista.)
+}
+void imprimirLista (Persona **lista) {
     //imprimir los valores de la lista enlazada
-    NodoPersona *actual = *lista;
+    Persona *actual = *lista;
     while (actual != NULL) {
-        printf("nombre: %s Edad: %d", actual->p.name,actual->p.edad);
+        printf("nombre: %s Edad: %d", actual->name,actual->edad);
         actual = actual->sig;
     }
-
 }
-
-
 int main(){
-/*
-    Lista nuevaLista;
-    inicializacion(nuevaLista);
 
-    //reservar espacio para los nodos
-    Nodo *cabeza = (Nodo*)malloc(sizeof (Nodo));
-    Nodo *nodoA = (Nodo*)malloc(sizeof (Nodo));
-    Nodo *nodoB = (Nodo*)malloc(sizeof (Nodo));
-    Nodo *nodoC = (Nodo*)malloc(sizeof (Nodo));
-    //asigacion de valores a los nodos
-    cabeza->sig = nodoA;
-    cabeza->valor = 10;
-    nodoA->sig = nodoB;
-    nodoA->valor = 15;
-    nodoB->sig = nodoC;
-    nodoB->valor = 20;
-    nodoC->sig = NULL;
-    nodoC->valor = 25;
-    }
-*/
-
-    NodoPersona lista = inicializarLista();
-    NodoPersona *nuevoNodo = crearNodo((char)"Gustavo", (int)37);
-    agregarNodoALista(*lista,*nuevoNodo);
-    imprimirLista(*lista);
+    Persona *pVoid;
+    pVoid = malloc(sizeof (Persona));
+    pVoid = crearPersona((char) "listaDePersonas", 01);
+    Persona *pPersona;
+    pPersona = malloc(sizeof (Persona));
+    pPersona = crearPersona((char) "Gustavo", 37);
+    agregarNodoALista(pVoid, pPersona);
+    imprimirLista(pVoid);
     return 0;
 }
-#pragma clang diagnostic pop
